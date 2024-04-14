1f7a1b630499501321a095b1360b1e3a9f3f6034
thaibaocoder1 <thaibaok4.1@gmail.com>
2024-04-15 00:20:48 +0700

    change url axios client

--- src/js/api/axiosClient.js
+++ src/js/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { toast } from '../utils'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: 'https://backend-ismart.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
